8ab8f17abdc780114ba0f39382e8f49dfa7ce458
Remy349 <santoraga15@gmail.com>
2023-01-14 23:00:49 -0600

    Added initial files and basic setup

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, ".env"))
+
+
+class Config(object):
+    secret_key = os.getenv("SECRET_KEY")
